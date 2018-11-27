f3e90894be358e47ef1eb81b517c916c4dfa4e7c
Bailey Diveley <bailey.diveley@gmail.com>
2018-11-27 13:49:42 -0700

    Move filter method into model and out of controller

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::Merchants::SearchController < ApplicationController
 
   def show
-    render json: Merchant.find_by("lower(name) = ?", params["name"].downcase) if params[:name]
-    render json: Merchant.find_by(created_at: params[:created_at]) if params[:created_at]
-    render json: Merchant.find_by(updated_at: params[:updated_at]) if params[:updated_at]
-    render json: Merchant.find_by(id: params[:id].to_i) if params[:id]
+    render json: Merchant.find_merchant(params)
+    # render json: Merchant.find_by("lower(name) = ?", params["name"].downcase) if params[:name]
+    # render json: Merchant.find_by(created_at: params[:created_at]) if params[:created_at]
+    # render json: Merchant.find_by(updated_at: params[:updated_at]) if params[:updated_at]
+    # render json: Merchant.find_by(id: params[:id].to_i) if params[:id]
   end
 
   def index
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
   def self.find_random
     Merchant.limit(1).order("RANDOM()").first
   end
+
+  def self.find_merchant(params)
+    binding.pry
+  end
 end
