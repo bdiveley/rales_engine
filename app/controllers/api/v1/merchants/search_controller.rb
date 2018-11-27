e2299a56b3d7e5129780e3fe8f8ae8588b031500
Bailey Diveley <bailey.diveley@gmail.com>
2018-11-27 15:39:03 -0700

    Refactor search controller for merchant

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,16 +1,15 @@
 class Api::V1::Merchants::SearchController < ApplicationController
 
   def show
-    render json: MerchantSerializer.new(Merchant.find_by("lower(name) = ?", params["name"].downcase)) if params[:name]
-    render json: MerchantSerializer.new(Merchant.find_by(created_at: params[:created_at])) if params[:created_at]
-    render json: MerchantSerializer.new(Merchant.find_by(updated_at: params[:updated_at])) if params[:updated_at]
-    render json: MerchantSerializer.new(Merchant.find_by(id: params[:id].to_i)) if params[:id]
+    render json: MerchantSerializer.new(Merchant.find_by(look_up_params))
   end
 
   def index
-    render json: MerchantSerializer.new(Merchant.where("lower(name) = ?", params["name"].downcase)) if params[:name]
-    render json: MerchantSerializer.new(Merchant.where(created_at: params[:created_at])) if params[:created_at]
-    render json: MerchantSerializer.new(Merchant.where(updated_at: params[:updated_at])) if params[:updated_at]
-    render json: MerchantSerializer.new(Merchant.where(id: params[:id].to_i)) if params[:id]
+    render json: MerchantSerializer.new(Merchant.where(look_up_params))
+  end
+
+private
+  def look_up_params
+    params.permit(:id, :name, :created_at, :updated_at)
   end
 end
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -4,14 +4,12 @@ namespace :import do
   task :data => :environment do
     CSV.foreach('db/csv/merchants.csv', :headers => true, header_converters: :symbol) do |row|
       Merchant.create(row.to_hash)
-      # id = row[0].to_i
-      # name = row[1]
-      # created_at = Time.parse(row[2])
-      # updated_at = Time.parse(row[3])
-      # Merchant.create(id: id, name: name, created_at: created_at, updated_at: updated_at)
     end
     CSV.foreach('db/csv/customers.csv', :headers => true, header_converters: :symbol) do |row|
       Customer.create(row.to_hash)
+    end
+    CSV.foreach('db/csv/items.csv', :headers => true, header_converters: :symbol) do |row|
+      Item.create(row.to_hash)
     end
   end
 end
