ec45bc9d28a246337895da5739b51810753c805f
Bailey Diveley <bailey.diveley@gmail.com>
2018-11-26 18:35:30 -0700

    Write test for find params

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,4 +10,25 @@ describe "Merchants API" do
     expect(response).to be_successful
     expect(merchants.count).to eq(5)
   end
+  it 'sends one merchant' do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/#{merchant.id}"
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item["id"]).to eq(merchant.id)
+    expect(item["name"]).to eq(merchant.name)
+  end
+  it 'finds one specific merchant based on find parameter' do
+    merchants = create_list(:merchant, 3)
+    first_merch = merchants[0]
+
+    get "/api/v1/merchants/find?name=#{first_merch.name}"
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(first_merch.id)
+    expect(merchant["name"]).to eq(first_merch.name)
+  end
 end
