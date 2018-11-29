4c4e43962397f7bfb8328fa45d289c7f9290a74d
Bailey Diveley <bailey.diveley@gmail.com>
2018-11-29 11:12:43 -0700

    complete AR logic for total revenue per merchanton a specific date" git status

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,13 +7,23 @@ class Invoice < ApplicationRecord
 
   validates_presence_of :status
 
-#GET /api/v1/merchants/revenue?date=x returns the total revenue for date x across all merchants
-  def total_revenue_by_date
-    Invoice.select("SUM(invoice_items.quantity*invoice_items.unit_price) AS revenue").joins(:invoice_items, :transactions).merge(Transaction.unscoped.successful).group("invoice_items.id").where("cast(invoices.created_at AS text) Like '2012-03-16%'").pluck("SUM(invoice_items.quantity*invoice_items.unit_price) AS revenue").sum
+#GET /api/v1/items/:id/best_day returns the date with the most sales for the given item using the invoice date.
+  def best_day
+    Invoice.select("invoices.created_at, sum(invoice_items.quantity) AS best_day").joins(:invoice_items).where("invoice_items.item_id = 1099").group("invoices.id").order("units desc, invoices.created_at desc").limit(1)
   end
 
-#GET /api/v1/items/:id/best_day returns the date with the most sales for the given item using the invoice date. If there are multiple days with equal number of sales, return the most recent day
-  def
-    Invoice.select("invoices.created_at, sum(invoice_items.quantity) AS units").joins(:invoice_items).where("invoice_items.item_id = 1099").group("invoices.id").order("units desc, invoices.created_at desc").limit(1)
+#GET /api/v1/merchants/:id/revenue returns the total revenue for that merchant across successful transactions
+  def total_revenue_by_merchant
+    Invoice.select("sum(invoice_items.quantity*invoice_items.unit_price) AS revenue").joins(:invoice_items, :transactions).group("invoice_items.id").where("transactions.result = ?", 'success').where("invoices.merchant_id = ?", 27).pluck("sum(invoice_items.quantity*invoice_items.unit_price) AS revenue").sum
+  end
+
+#GET /api/v1/merchants/revenue?date=x returns the total revenue for date x across all merchants
+  def total_revenue_by_date
+    Invoice.select("SUM(invoice_items.quantity*invoice_items.unit_price) AS revenue").joins(:invoice_items, :transactions).group("invoice_items.id").where("transactions.result = ?", 'success').where("cast(invoices.created_at AS text) Like '2012-03-16%'").pluck("SUM(invoice_items.quantity*invoice_items.unit_price) AS revenue").sum
+  end
+
+#GET /api/v1/merchants/:id/revenue?date=x returns the total revenue for that merchant for a specific invoice date x
+  def total_revenue_per_merchant_by_date
+    Invoice.select("SUM(invoice_items.quantity*invoice_items.unit_price) AS revenue").joins(:invoice_items, :transactions).group("invoices.id, invoices.merchant_id").where("transactions.result = ?", 'success').where("cast(invoices.created_at AS text) Like '2012-03-07%'").where("invoices.merchant_id = ?", 3)
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,11 +3,6 @@ class Merchant < ApplicationRecord
   has_many :invoices
   validates_presence_of :name
 
-#GET /api/v1/merchants/most_revenue?quantity=x returns the top x merchants ranked by total revenue
-  def
-
-  end
-
 #GET /api/v1/merchants/most_items?quantity=x returns the top x merchants ranked by total number of items sold
   def top_merchants_by_items_sold
     Merchant.select("merchants.*, sum(invoice_items.quantity) AS items_sold").joins(:invoices).joins("JOIN invoice_items ON invoice_items.invoice_id = invoices.id").joins("JOIN transactions ON transactions.invoice_id = invoices.id").where("transactions.result = ?", 'success').group("merchants.id").order("items_sold DESC").limit(8)
@@ -17,4 +12,5 @@ class Merchant < ApplicationRecord
   def top_merchants_by_total_revenue
     Merchant.select("merchants.*, sum(invoice_items.quantity*invoice_items.unit_price) AS revenue").joins(:invoices).joins("JOIN invoice_items ON invoice_items.invoice_id = invoices.id").joins("JOIN transactions ON transactions.invoice_id = invoices.id").where("transactions.result = ?", 'success').group("merchants.id").order("revenue DESC").limit(8)
   end
+
 end
