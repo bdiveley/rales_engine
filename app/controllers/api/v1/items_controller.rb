d463df01530934b07b8519205918d6a1fe25c28f
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-01 11:03:35 -0700

    Fix one failing test

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    render json: ItemSerializer.new(Item.all)
+    unless look_up_params.empty?
+      render json: ItemSerializer.new(Item.where(look_up_params))
+    else
+      render json: ItemSerializer.new(Item.all)
+    end
   end
 
   def show
    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+private
+  def look_up_params
+    params.permit(:merchant_id)
   end
 end
